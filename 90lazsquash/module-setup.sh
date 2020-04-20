4e884b0f14c97981a9b05c95e007c6feff10bccd
Joseph Voss <jvoss@josephvoss.com>
2020-04-19 22:32:57 -0400

    Typo fix

--- 90lazsquash/module-setup.sh
+++ 90lazsquash/module-setup.sh
@@ -8,7 +8,7 @@ check() {
 depends() {
   # We have no deps
   # Scratch that. Img-lib would be nice to get tar
-  echo img-lib
+  echo base img-lib
   return 0
 }
 
@@ -20,7 +20,7 @@ install() {
   # Rsync is needed to build overlay from squash
   # blkid is needed to build mount dev
   inst_multiple blkid
-  int_hook cmdline 90 "$moddir/parse-lazsquash.sh"
-  int_hook mount 90 "$moddir/mount-lazsquash.sh"
+  inst_hook cmdline 90 "$moddir/parse-lazsquash.sh"
+  inst_hook mount 90 "$moddir/mount-lazsquash.sh"
 }
 # vim: set ts=2 sw=2 tw=0 et :
