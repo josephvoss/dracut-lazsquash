58c041f67f31b2abdd8da6176b4619750b680d64
Joseph Voss <jvoss@josephvoss.com>
2020-07-18 18:47:53 -0400

    Build new kernel

--- add_image_to_usb.sh
+++ add_image_to_usb.sh
@@ -8,10 +8,10 @@ mount_path=$(buildah mount $container)
 
 mksquashfs "$mount_path" "$output_path/$image"
 
-#buildah copy $container ./90lazsquash /usr/lib/dracut/90lazsquash
+#buildah copy $container ./90lazsquash /usr/lib/dracut/modules.d/90lazsquash
 #
-#buildah run $container dnf install -y kernel dracut
+#buildah run $container dnf install -y kernel dracut-network dracut-live
 #kernel=$(buildah run $container  bash -c "rpm -q kernel | cut -d '-' -f 2 | sort -r")
 #
-#buildah run $container dracut --no-hostonly --modules "bash lazsquash network kernel-modules kernel-modules-extra" /initrd-$kernel $kernel
+#buildah run --mount type=bind,src=/tmp,target=/var/tmp $container dracut --no-hostonly --modules "bash lazsquash network kernel-modules kernel-modules-extra" /initrd-$kernel $kernel
 #rsync -avz $mount_path/boot/vmlinuz-${kernel}-* $output_path
