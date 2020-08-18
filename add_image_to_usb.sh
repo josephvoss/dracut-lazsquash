6ccdbb07cc748dc80073ce040a11226a2cd18c51
Joseph Voss <jvoss@josephvoss.com>
2020-08-17 23:30:08 -0400

    Add root image to usb

--- add_image_to_usb.sh
+++ add_image_to_usb.sh
@@ -6,6 +6,9 @@
 image=jvoss-user:f01055c
 image=fedora-desktop-32-x86_64:latest
 image=dotfiles:e054b24
+image=jvoss-user:af221f5
+image=centos-zfs-8.2.2004-x86_64:latest
+image=root-user:b72a491
 
 disk_mount=/dev/disk/by-partlabel/IMAGES
 mount_point=$(mktemp -d)
@@ -15,7 +18,7 @@ container=$(buildah from $image) &&
 mount_path=$(buildah mount $container) &&
 mount "${disk_mount}" "${mount_point}" &&
 
-mksquashfs "$mount_path" "$output_path/$output_image" &&
+mksquashfs "$mount_path" "$mount_point/$output_image" &&
 umount "${mount_point}" && rm -rf "${mount_point}" &&
 
 buildah rm $container
